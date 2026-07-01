cdd47d268eca1e83c6fa4e8557a01c03f0464b7e
musyafak-a <f.yusril703@gmail.com>
2026-07-01 11:21:14 +0700

    html1

--- first_app/urls.py
+++ first_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('student_create', views.create_student, name='student create'),
+    path('detail/<int:id>/', views.detail, name='detail')
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import student
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the first_app index.")
+    datas = student.objects.all()
+    return HttpResponse(datas)
+
+def create_student(request):
+    data = student(name='John', age=20, number_student=12345)
+    data.save()
+    return HttpResponse("successfully.")
+
+def detail(request, id):
+    #  return HttpResponse("show data %s" %id)
+
+    template = loader.get_template('detail.html')
+    context = {}
+
+    return HttpResponse(template.render(context, request))
