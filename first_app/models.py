bb20ad2f26e4f342fe1b2c4c1454ce60db216c6a
musyafak-a <f.yusril703@gmail.com>
2026-07-01 10:11:31 +0700

    confi dtaabase postgre

--- first_app/models.py
+++ first_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    created_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
--- projek1/settings.py
+++ projek1/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'first_app.apps.FirstAppConfig',  # Add the first_app to the installed apps
+
 ]
 
 MIDDLEWARE = [
@@ -74,8 +76,14 @@ WSGI_APPLICATION = 'projek1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'projek1_db',
+        'USER': 'postgres',
+        'PASSWORD': 'knjtbadak',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
