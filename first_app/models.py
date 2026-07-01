8eb7fb5f5b391ae7f2c6ceabe55fde680821d48d
musyafak-a <f.yusril703@gmail.com>
2026-07-01 10:23:37 +0700

    migrate

--- first_app/models.py
+++ first_app/models.py
@@ -5,6 +5,7 @@ class student(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
     created_time = models.DateTimeField(auto_now=True)
+    number_student = models.IntegerField(max_length=10,null=True)
 
     def __str__(self):
         return self.name
